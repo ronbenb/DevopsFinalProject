331f0f0e32a4dda3cce15799559bbedd88de042c
ronbenb <ronbenb@gmail.com>
2023-07-10 20:05:22 +0300

    Update project files

--- src/main/java/com/dev/Devapp/controller.java
+++ src/main/java/com/dev/Devapp/controller.java
@@ -3,6 +3,12 @@ package com.dev.Devapp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @RestController
 public class controller {
 
@@ -11,16 +17,28 @@ public class controller {
     public controller(LoggingService loggingService) {
         this.loggingService = loggingService;
     }
-    @GetMapping("/")
-    public String index1() {
-        loggingService.logEndpointAccess("/");
-        return "Greetings from Spring Boot!";
-    }
 
     @GetMapping("/logs")
-    public String index() {
+    public String getLogs() {
         loggingService.logEndpointAccess("/logs");
-        return "logs";
+        String logFilePath = "/logs/file.log";
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(logFilePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append("<br>");
+            }
+        } catch (IOException e) {
+            return "Failed to read log file: " + e.getMessage();
+        }
+        return stringBuilder.toString();
+    }
+        @GetMapping("/transactions")
+    public String index1() {
+        loggingService.logEndpointAccess("/transactions");
+        return "transactions";
     }
 
-}
+
+    }
+
